e2e97fecabbc613ac0ebd3762b2a4c715658f712
jgriss <jgriss@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2014-12-19 13:38:36 +0000

    jg - increased thresholds to match original algorithm

--- src/main/java/uk/ac/ebi/pride/spectracluster/util/Defaults.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/util/Defaults.java
@@ -32,13 +32,13 @@ public class Defaults {
      * Defines the similarity threshold above which spectra are added
      * to a cluster.
      */
-    public static final double DEFAULT_SIMILARITY_THRESHOLD = 0.6;
+    public static final double DEFAULT_SIMILARITY_THRESHOLD = 0.7;
 
     /**
      * Defines the similarity threshold below which spectra are removed
      * from a cluster.
      */
-    public static final double DEFAULT_RETAIN_THRESHOLD = 0.5;
+    public static final double DEFAULT_RETAIN_THRESHOLD = 0.6;
 
     public static final int DEFAULT_LARGE_BINNING_REGION = 1000;
 
