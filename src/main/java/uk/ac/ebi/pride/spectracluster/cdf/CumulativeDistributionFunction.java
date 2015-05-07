55d4b636634fc95e0c8386f1a66a5d01c3b12c49
Johannes Griss <jgriss@users.noreply.github.com>
2015-05-07 10:14:06 +0200

    Added support for CumulativeDistributionFunctions
    These classes can be used to derive p-values for similarity matches based
    on empirical distributions. These empirical distributions must be available
    as text files. The CumulativeDistributionFunction class also provides a function
    whether a spectrum can be merged with a cluster without breaching an estimated
    clustering accuracy (number of misclassified spectra).

--- src/main/java/uk/ac/ebi/pride/spectracluster/cdf/CumulativeDistributionFunction.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/cdf/CumulativeDistributionFunction.java
@@ -1,7 +1,110 @@
 package uk.ac.ebi.pride.spectracluster.cdf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Represents the empirical cumulative distribution
+ * function for a certain similarity metric.
+ *
  * Created by jg on 05.05.15.
  */
 public class CumulativeDistributionFunction {
+    public static final String HEADER_LINE = "max_score\tlower_diff_matches\tcum_lower_diff_matches\trel_cum_lower_matches\ttotal_matches";
+    protected final long totalComparisons;
+    protected final double scoreIncrements;
+    /**
+     * Stores the relative number of peptides that were observed
+     * below the score threshold. The score threshold for a given
+     * array position <i>i</i> is calculated using
+     * <i>(i + 1) * scoreIncrements</i>
+     */
+    protected final List<Double> proportionPeptidesBelowScore;
+
+    public CumulativeDistributionFunction(long totalComparisons, double scoreIncrements, List<Double> proportionPeptidesBelowScore) {
+        this.totalComparisons = totalComparisons;
+        this.scoreIncrements = scoreIncrements;
+        this.proportionPeptidesBelowScore = proportionPeptidesBelowScore;
+    }
+
+    public static CumulativeDistributionFunction fromString(String string) throws Exception {
+        if (string == null || string.length() < 1)
+            return null;
+
+        // split into lines
+        String[] lines = string.split("\n");
+
+        if (lines.length < 2)
+            return null;
+
+        // sanity check whether the right format is used
+        if (!HEADER_LINE.equals(lines[0]))
+            throw new Exception("Unsupported format used. Header line does not match.");
+
+        String[] firstFields = lines[1].split("\t");
+        if (firstFields.length != 5)
+            throw new Exception("First line does not contain the expected 5 fields.");
+
+        // save the total comparisons and score increment
+        long totalComparisons = Long.parseLong(firstFields[4]);
+        double scoreIncrement = Double.parseDouble(firstFields[0]);
+
+        // get the actual values
+        List<Double> proportionPeptidesBelowScore = new ArrayList<Double>();
+        for (int i = 1; i < lines.length; i++) {
+            String line = lines[i];
+            String[] fields = line.split("\t");
+
+            if (fields.length != 5)
+               throw new Exception("Line does not contain the expected 5 fields.");
+
+            long numberCumulativePeptides = Long.parseLong(fields[2]);
+
+            double relCumulativePeptides = (double) numberCumulativePeptides / totalComparisons;
+
+            proportionPeptidesBelowScore.add(relCumulativePeptides);
+        }
+
+        return new CumulativeDistributionFunction(totalComparisons, scoreIncrement, proportionPeptidesBelowScore);
+    }
+
+    protected int getBinForScore(double score) {
+        double doubleBin = score / scoreIncrements;
+
+        // use ceil to get the next higher scoring bin
+        int index = (int) Math.ceil(doubleBin);
+
+        if (index >= proportionPeptidesBelowScore.size())
+            index = proportionPeptidesBelowScore.size() - 1;
+
+        return index;
+    }
+
+    public double getCdfForThreshold(double threshold) {
+        int index = getBinForScore(threshold);
+
+        return proportionPeptidesBelowScore.get(index);
+    }
+
+    public double probability(double threshold, int nComparisons) {
+        double pValue = 1.0 - Math.pow(getCdfForThreshold(threshold), nComparisons);
+
+        return pValue;
+    }
+
+    /**
+     * Determines whether the match at the given similarity would be sufficiently good
+     * to satisfy the defined maximal mixture probability.
+     * @param similarity Similarity of the match
+     * @param nComparisons Number of comparisons already performed for the spectrum.
+     * @param maximumMixtureProbability Allowed maximum mixture probability.
+     * @return
+     */
+    public boolean isSaveMatch(double similarity, int nComparisons, double maximumMixtureProbability) {
+        // get the estimated proportion of correct matches at this threshold / similarity
+        double proportionCorrectMatches = Math.pow(getCdfForThreshold(similarity), nComparisons);
+        double minimumCorrectMatches = 1.0 - maximumMixtureProbability;
+
+        return proportionCorrectMatches > minimumCorrectMatches;
+    }
 }
--- src/test/java/uk/ac/ebi/pride/spectracluster/cdf/TestCumulativeDistributionFunction.java
+++ src/test/java/uk/ac/ebi/pride/spectracluster/cdf/TestCumulativeDistributionFunction.java
@@ -1,7 +1,25 @@
 package uk.ac.ebi.pride.spectracluster.cdf;
 
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.ebi.pride.spectracluster.similarity.CombinedFisherIntensityTest;
+
 /**
  * Created by jg on 05.05.15.
  */
 public class TestCumulativeDistributionFunction {
+    @Test
+    public void testCombinedCdf() throws Exception {
+        CumulativeDistributionFunction cumulativeDistributionFunction =
+                CumulativeDistributionFunctionFactory.getCumulativeDistributionFunctionForSimilarityMetric(
+                        CombinedFisherIntensityTest.class);
+
+        Assert.assertEquals(0.0, cumulativeDistributionFunction.probability(100, 4), 0);
+        Assert.assertEquals(8.385432087587574E-7, cumulativeDistributionFunction.probability(60, 4), 0);
+
+        Assert.assertTrue(cumulativeDistributionFunction.isSaveMatch(100, 4, 0.01));
+        Assert.assertTrue(cumulativeDistributionFunction.isSaveMatch(60, 4, 0.01));
+        Assert.assertFalse(cumulativeDistributionFunction.isSaveMatch(60, 40000000, 0.01));
+        Assert.assertFalse(cumulativeDistributionFunction.isSaveMatch(30, 4000, 0.01));
+    }
 }
