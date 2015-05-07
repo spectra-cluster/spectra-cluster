badc5969753fa11ddbf9f8a471b25b59eb26e8b8
Johannes Griss <jgriss@users.noreply.github.com>
2015-05-07 10:17:28 +0200

    Added class ComparisonMatch.
    This helper class is used to store the similarity value to another spectrum based
    on the spectrum's id.

--- src/main/java/uk/ac/ebi/pride/spectracluster/util/ComparisonMatch.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/util/ComparisonMatch.java
@@ -1,7 +1,34 @@
 package uk.ac.ebi.pride.spectracluster.util;
 
 /**
+ * This class only stores a certain similarity and the
+ * spectrum id it was associated with. It is intended to
+ * store similarity matches within a cluster.
+ *
  * Created by jg on 06.05.15.
  */
-public class ComparisonMatch {
+public class ComparisonMatch implements Comparable<ComparisonMatch> {
+    private final String spectrumId;
+    /**
+     * Single precision is sufficient for this
+     */
+    private final float similarity;
+
+    public ComparisonMatch(String spectrumId, float similarity) {
+        this.spectrumId = spectrumId;
+        this.similarity = similarity;
+    }
+
+    public String getSpectrumId() {
+        return spectrumId;
+    }
+
+    public float getSimilarity() {
+        return similarity;
+    }
+
+    @Override
+    public int compareTo(ComparisonMatch o) {
+        return Float.compare(this.similarity, o.similarity);
+    }
 }
