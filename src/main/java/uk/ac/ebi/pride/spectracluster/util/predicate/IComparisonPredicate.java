fc93bde8da9a37c5f83db1e93dce0c76cba56731
Johannes Griss <jgriss@users.noreply.github.com>
2015-05-20 08:02:58 +0200

    Added new Predicate that compares two objects

---
+++ src/main/java/uk/ac/ebi/pride/spectracluster/util/predicate/IComparisonPredicate.java
@@ -0,0 +1,10 @@
+package uk.ac.ebi.pride.spectracluster.util.predicate;
+
+/**
+ * Compares two spectra
+ *
+ * Created by jg on 20.05.15.
+ */
+public interface IComparisonPredicate<T> {
+    public boolean apply(T o1, T o2);
+}
